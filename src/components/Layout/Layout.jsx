a3d4717da82eaba4b7bf3cacb94825e7f9b05713
Sazzad <sazzad47.ju@gmail.com>
2023-09-13 08:51:04 +0600

    make it better

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -4,12 +4,16 @@ import Footer from "./Footer";
 import Navbar from "./Navbar";
 const Layout = ({ children }) => {
   return (
-    <>
+    <div style={{minHeight: "100vh"}}>
       <Header />
       <Navbar />
+      <div style={{minHeight: "50vh", marginBottom: "2rem"}}>
       {children}
+      </div>
+      <div>
       <Footer />
-    </>
+      </div>
+    </div>
   );
 };
 
